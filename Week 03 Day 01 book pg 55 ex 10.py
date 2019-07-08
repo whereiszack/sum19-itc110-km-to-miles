adde4510a45810560433e8d3ae51007878dec92e
whereiszack <zobertson@gmail.com>
2019-07-08 11:51:09 -0700

    Add files via upload
    Fixed some spacing issues in the output and properly aligned the closing "main" tag

--- Week 03 Day 01 book pg 55 ex 10.py
+++ Week 03 Day 01 book pg 55 ex 10.py
@@ -4,6 +4,6 @@ def main():
     print("This program converts Kilometers to Miles")
     km = float(input("Type the number of kilometers: "))
     miles = km * 31/50
-    print(km, "= ", miles)
+    print(km, "km  = ", miles, "miles")
 
-    main()
+main()

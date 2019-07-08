744413b11e97bf9a6be00a1d171424a9e9f1dd0a
whereiszack <zobertson@gmail.com>
2019-07-08 11:54:16 -0700

    Add files via upload
    added some line notes

--- Week 03 Day 01 book pg 55 ex 10.py
+++ Week 03 Day 01 book pg 55 ex 10.py
@@ -1,9 +1,9 @@
 #input number of kilometers and convert the number to miles.
 
 def main():
-    print("This program converts Kilometers to Miles")
-    km = float(input("Type the number of kilometers: "))
-    miles = km * 31/50
-    print(km, "km  = ", miles, "miles")
+    print("This program converts Kilometers to Miles") 
+    km = float(input("Type the number of kilometers: "))#get user input
+    miles = km * 31/50 #31/50 is the fraction eqiuvalent of .62
+    print(km, "km  = ", miles, "miles") #final output of converted km to miles
 
 main()

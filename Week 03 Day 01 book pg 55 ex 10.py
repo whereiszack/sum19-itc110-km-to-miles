40f487edd440af0638ecc4a5fa549b344596673a
whereiszack <zobertson@gmail.com>
2019-07-08 11:46:32 -0700

    Add files via upload
    First attempt at converting km to miles

---
+++ Week 03 Day 01 book pg 55 ex 10.py
@@ -0,0 +1,9 @@
+#input number of kilometers and convert the number to miles.
+
+def main():
+    print("This program converts Kilometers to Miles")
+    km = float(input("Type the number of kilometers: "))
+    miles = km * 31/50
+    print(km, "= ", miles)
+
+    main()
